063934237af03a271b0ea10c1861342e3c03afcc
Aida Bedaj <aida_bedaj@universitetipolis.edu.al>
2025-06-08 16:31:41 +0200

    editime

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,7 +3,6 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     id ("kotlin-android")
-    id ("kotlin-android-extensions")
 }
 
 android {
@@ -38,6 +37,7 @@ android {
     }
     buildFeatures {
         compose = true
+        viewBinding = true
     }
 }
 
